1d9384eabab3b67831f1521be35714232e49134c
Olga <olga.sleepless@gmail.com>
2023-02-10 15:22:41 -0500

    Add it_was_ok, harrison_ford

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -6,6 +6,8 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
+  Movie.where(score: 2..3).select('id', 'title', 'score')
+  #Movie.where(score: 2..3).select(:id, :title, :score) # alternative
 
 end
 
@@ -20,6 +22,7 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
+  Movie.joins(:actors).where(actors: {name: 'Harrison Ford'}).where.not(castings: {ord: 1}).select(:id, :title)
 
 end
 
